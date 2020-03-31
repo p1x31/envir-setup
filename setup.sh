74d872e4dba063984a24d81cd70275927e17c734
Your Name <vadim_makarenkov@mail.ru>
2020-03-31 19:05:12 +0300

    bash file to setup environment

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+
+sudo rm /etc/apt/apt.conf.d/*.*
+sudo apt update
+sudo apt instal git
+mkdir downloads
+cd downloads
+wget https://repo.continuum.io/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh
+cd
+git clone https://github.com/fastai/fastai.git
+cd fastai/
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+export PATH=~/anaconda3/bin:$PATH
+source ~/.bashrc
+conda env update
+echo 'source activate fastai' >> ~/.bashrc
+source activate fastai
+source ~/.bashrc
+cd ..
+mkdir data
+cd data
+wget http://files.fast.ai/data/dogscats.zip
+sudo apt install unzip -y
+sudo apt -y upgrade --force-yes
+sudo apt -y autoremove
+unzip -q dogscats.zip
+cd ../fastai/courses/dl1/
+ln -s ~/data ./
+jupyter notebook --generate-config
+echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+sudo ufw allow 8888/tcp
+sudo apt -y install qtdeclarative5-dev qml-module-qtquick-controls
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update
+cd ~/downloads/
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1810/x86_64/cuda-repo-ubuntu1810_10.1.168-1_amd64.deb
+dpkg -i cuda-repo-ubuntu1810_10.1.168-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt update
+sudo apt install cuda -y
+wget http://files.fast.ai/files/cudnn-10.2-linux-x64-v7.6.5.32.tgz
+tar xf cudnn-10.2-linux-x64-v7.6.5.32.tgz
+sudo cp cuda/include/*.* /usr/local/cuda/include/
+sudo cp cuda/lib64/*.* /usr/local/cuda/lib64/
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+sudo reboot
